c7914bd8d3b6d9b3db2beb8b6d810824d9b5190d
rafaelg7 <138294493+rafaelg7@users.noreply.github.com>
2023-07-03 20:19:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command()
+async def hallo(ctx):
+  await ctx.send('Hallo, wie geht es dir. Ich bin der neue Bot auf diesem Server!')
+
+bot.run('MTEyNDMyODA3NDkyNjg4NzAwMg.GDjSdp.4cjQGNHDZutt5CLIkd6DJZbshGLQzivGIURwQ0')
